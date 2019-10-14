3c46afbe1d157cf6887eaccb72c666fe2600104d
Riley Moore <rilemoore@gmail.com>
2019-10-14 20:40:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
 def my_collect
+  
+  i = 0
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
+  array
+  # code here
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+   i
+  
 end
