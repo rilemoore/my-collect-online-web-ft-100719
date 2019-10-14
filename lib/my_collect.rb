c5b6b363765b73605928d05709d059e8b75fa863
Riley Moore <rilemoore@gmail.com>
2019-10-14 20:43:08 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,11 @@
 def my_collect(array)
-  new_array
+  new_array = []
   i = 0
   while i < array.length
-    new_array << yield array[i]
+    new_array = yield array[i]
     i = i + 1
   end
-  new_array
+  array
   # code here
 end
 
